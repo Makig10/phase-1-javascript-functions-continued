96aaa470898941f50446e64b327f5d0bbe3e1b13
Brian <mahuibrian14@gmail.com>
2023-03-23 13:12:36 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(action='roller-skate'){
+    return (`This Saturday, I want to ${action}`);
+}
+
+function mondayWork(action='go to the office'){
+    return (`This Monday, I will ${action}`);
+
+}
+//console.log(function mondayWork(action='work from home'));
+
+
+//const array = (function (thingToAdd) {
+    //const base = 3;
+    //return [
+      //function () {
+        //return base + thingToAdd;
+      //},
+      //function () {
+        //return base;
+      //},
+   // ];
+  //})(2);
+ // array[0];
+ // array[1];
+ function wrapAdjective('*') {
+    return function(adjective) {
+      return `You are ${'*'}${adjective}${'*'}!`;
+    }
+  }
